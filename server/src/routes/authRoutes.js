eea199172dc3d0ddaff82f85e75237b6a9f35556
carloospg <carlosperezgosalvez@gmail.com>
2026-03-25 17:18:25 +0100

    feat: añado la ruta de eliminar

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -8,3 +8,4 @@ export const router = Router();
 router.post('/login', controlador.login);
 router.post('/registro', verificarToken, verificarRol('admin'), controlador.registrar);
 router.get('/usuarios', verificarToken, verificarRol('admin'), controlador.listar);
+router.delete('/usuarios/:id', verificarToken, verificarRol('admin'), controlador.eliminar);
