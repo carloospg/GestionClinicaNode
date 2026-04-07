f190b0a0301f115e75d387f084b50782016dc9c3
carloospg <carlosperezgosalvez@gmail.com>
2026-04-07 14:32:43 +0200

    feat: añado a las rutas el listar los pacientes

--- server/src/routes/pacienteRoutes.js
+++ server/src/routes/pacienteRoutes.js
@@ -5,4 +5,5 @@ import { verificarToken } from "../middlewares/authMiddleware.js";
 
 export const router = Router();
 
-router.post('/', verificarToken, verificarRol('admin', 'recepcionista'), controlador.crear)
+router.post('/', verificarToken, verificarRol('admin', 'recepcionista'), controlador.crear);
+router.get('/', verificarToken, controlador.listar);
