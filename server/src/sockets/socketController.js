30696f949a7cf0ce05c4d79270ca0c0e9739b1b0
carloospg <carlosperezgosalvez@gmail.com>
2026-04-13 21:24:57 +0200

    feat: añado un medico y un recepcionista al seeder para poder probar

--- server/src/sockets/socketController.js
+++ server/src/sockets/socketController.js
@@ -2,11 +2,8 @@ export const socketController = (socket, io) => {
 
   socket.on('unirse-sala', (id_usuario) => {
     socket.join(`usuario-${id_usuario}`);
-    console.log(`Usuario ${id_usuario} se unio a su sala`);
   });
 
-  socket.on('disconnect', () => {
-    console.log(`Cliente desconectado: ${socket.id}`);
-  });
+  socket.on('disconnect', () => {});
 
 };
